3dd08b65094eda5ec97c61affb4d0af83bd76575
nahar <naharsignup@gmail.com>
2026-08-15 15:24:43 +0530

    division feature

--- app.py
+++ app.py
@@ -4,3 +4,4 @@ num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
 print("Result:", utils.calculator.add(num1, num2))
+print("division Result:", utils.calculator.divide(num1, num2))
--- utils/calculator.py
+++ utils/calculator.py
@@ -7,6 +7,10 @@ def subtract(a, b):
 
 def multiply(a, b): 
     return a * b
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    return a / b
 
 if __name__ == "__main__":
     num1 = float(input("Enter first number: "))
