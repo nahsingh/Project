4eca20167e25b150efd6f15cc0900553778d5578
nahar <naharsignup@gmail.com>
2026-08-15 18:15:35 +0530

    hellow world add

--- app.py
+++ app.py
@@ -5,3 +5,4 @@ num2 = float(input("Enter second number: "))
 
 print("Result:", utils.calculator.add(num1, num2))
 print("division Result:", utils.calculator.divide(num1, num2))
+print("Hello World!")
