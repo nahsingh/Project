1bb5be3d3df64dd25d8bff0535bf619f5a16c5af
nahar <naharsignup@gmail.com>
2026-08-15 15:33:58 +0530

    unknown1

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
 print("Result:", utils.calculator.add(num1, num2))
-print("Hello, World! This is the main application.")
+print("division Result:", utils.calculator.divide(num1, num2))
