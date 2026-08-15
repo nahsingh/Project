2fcf00bddba8f8cc5a5b304639d00cf1c80025f1
nahar <naharsignup@gmail.com>
2026-08-15 15:20:46 +0530

    adding caluclator app

---
+++ app.py
@@ -0,0 +1,6 @@
+import utils.calculator
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Result:", utils.calculator.add(num1, num2))
---
+++ utils/calculator.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b): 
+    return a * b
+
+if __name__ == "__main__":
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    print("Addition:", add(num1, num2))
+    print("Subtraction:", subtract(num1, num2))
+    print("Multiplication:", multiply(num1, num2))
